1937007f167d326bf273d839a891d4d70c15f525
Mzoro <zorkiy2000@gmail.com>
2016-07-29 17:44:50 +0300

    add components

---
+++ client/app/bundles/HelloWorld/components/CreateProd.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class CreateProd extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Create Post</h1>
+      </div>
+    );
+  }
+}
---
+++ client/app/bundles/HelloWorld/components/IndexProd.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class IndexProd extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Create Post</h1>
+      </div>
+    );
+  }
+}
